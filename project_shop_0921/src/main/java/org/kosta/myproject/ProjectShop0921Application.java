c4d7a6727630026ab28b4231a0f35d8df4a16df7
varchar213 <jmcs7eg1@gmail.com>
2023-09-21 15:53:20 +0900

    초기 스프링 부트 -> git에 이식 완료

---
+++ project_shop_0921/src/main/java/org/kosta/myproject/ProjectShop0921Application.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShop0921Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShop0921Application.class, args);
+	}
+
+}
